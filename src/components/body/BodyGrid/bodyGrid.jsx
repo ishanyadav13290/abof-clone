281db12c72883ed9c7dc3d704aeae6281a47729a
ishanyadav13290 <ishanyadav13290@gmail.com>
2023-01-17 18:46:52 +0530

    grid responsiveness added

--- src/components/body/BodyGrid/bodyGrid.jsx
+++ src/components/body/BodyGrid/bodyGrid.jsx
@@ -4,7 +4,9 @@ import { GridImgs } from "../../../data/db";
 export default function BodyGrid(){
     return <Box p="50px">
         <Center><Text fontSize="2xl" fontWeight={700}>Top Categories</Text></Center>
-        <Grid templateColumns='repeat(5, 1fr)' gap={6} m="auto" mt="10" width="90%">
+        {/* <Grid templateColumns='repeat(5, 1fr)' gap={6} m="auto" mt="10" width="90%" style={Styles}> */}
+        <Grid gridTemplateColumns={["repeat(2, 1fr)", "repeat(3, 1fr)", "repeat(5, 1fr)"]} gap={6} m="auto" mt="10" width="90%">
+        
         {GridImgs.map((elem,i)=>{
             return <GridItem key={i} m="auto"><Image src={elem} width="95%" /></GridItem>
         })}
